0ac0d5cf010729c1054bb12ff46ff5a34be02711
haducacarlo1220 <ralphhaduca@gmail.com>
2024-09-08 14:31:38 +0800

    initial commit with app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+//Variables
+console.log("Hello, World!");
+let name = "Ralph Carlo H. Pano";
+console.log('My name is ' + name);
+
+//Numbers
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2;
+console.log(sum)
+
+//Booleans
+let isActive = true;
+let inSubscribed = false;
+console.log(isActive);
+console.log(inSubscribed);
+
+//Comparison-operator
+let A = 12;
+let B = 20;
+
+let isAGreaterThanB = A > B;
+let isALessThanB = A < B;
+let isAEqualToB = A == B;
+let isANotEqualToB = A !== B;
+
+console.log('Is A greater than B?', isAGreaterThanB);
+console.log('Is A less than B?', isALessThanB);
+console.log('Is A equal to B?', isAEqualToB);
+console.log('Is A not equal to B?', isANotEqualToB);
